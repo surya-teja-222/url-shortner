2e0d568f212b09dd9f1ba5ccc553f0e719c2381d
surya-teja-222 <e20cse185@bennett.edu.in>
2021-12-22 11:51:50 +0530

    header-mob-all-sizes

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,61 @@
 import './Header.css';
+import logo from './assets/images/logo.svg';
+
+
 
 function Header(){
+
+    function handleClick() { 
+        document.getElementById('hamid').classList.toggle('ham');
+        document.getElementById('dropdown').classList.toggle('hidden');
+    }
+
     return (
-        <div className="header">
-            <h1>Header</h1>
+        <div className="header font-Poppins align-middle">
+            
+            <div className='flex w-full justify-between  mt-[40px] '>
+                <div className='flex text-t-grey'>
+                    <img src={logo} className='w-[120px] h-[34px] mdm:ml-[24px] md:ml-[165px]' alt='AppLogo'/>
+
+                    <div className='ch-1 mdm:hidden ml-[45px] font-medium text-[18px]'  >
+                        <a className='mr-[30px] hover:text-c-very-dark-blue hover:cursor-pointer' href='#' >Features</a>
+                        <a className='mr-[30px] hover:text-c-very-dark-blue hover:cursor-pointer' href='#' >Pricing</a>
+                        <a className='mr-[0px]  hover:text-c-very-dark-blue hover:cursor-pointer' href='#' >Resources</a>
+                    </div>
+                </div>
+
+                <div className='ch-2  text-t-grey mdm:hidden md:mr-[165px] font-medium text-[18px]' >
+                        <a href='#' className='hover:text-c-very-dark-blue hover:cursor-pointer'>Login</a>
+                        <a className='ml-[38px] green-button ' href='#' >Sign Up</a>
+                </div>
+
+                <div className="w-[24px] h-[24px] mr-[24px] md:hidden " id='hamid' onClick={handleClick} >
+                    <div className="ham-line ham-animation-1 h-[3px] bg-t-grey my-[3px]" ></div>
+                    <div className="ham-line ham-animation-2 h-[3px] bg-t-grey my-[3px]" ></div>
+                    <div className="ham-line ham-animation-3 h-[3px] bg-t-grey my-[3px]" ></div>
+                </div>
+            </div>
+
+            <div className='absolute dropdown text-c-white font-bold hidden ease-in duration-300' id='dropdown'>
+                <div className='flex flex-col'>
+                    <a href='#' className='self-center mt-[38px]' >Features</a>
+                    <a href='#' className='self-center mt-[38px]'>Pricing</a>
+                    <a href='#' className='self-center my-[38px]'>Resources</a>
+                </div>
+                <center><hr className='w-[280px] self-center '></hr></center>
+                <div className='flex flex-col'>
+                <a href='#' className='self-center mt-[38px] '>Login</a>
+                <a href='#' className='green-button self-center mt-[38px]'>Sign Up</a>
+                </div>
+
+            </div>
+
         </div>
     );
 }
 
 
+
 export default Header;
+
+// LOGO , HAM {Features,  Pricing , Resources , <hr> , Login , Sign Up}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '375px',
+      'md': '1040px',
+      'lg': '1440px', 
+      'smm': {'max': '375px'},
+      'mdm': {'max': '1040px'},
+      'lgm': {'max': '1440px'},
+    },
+    // custom colors
+    colors: {
+      't-grey': 'hsl(0, 0%, 75%)',
+      'c-cyan'  : 'hsl(180, 66%, 49%)',
+      'c-white' : 'hsl(0, 0%, 100%)',
+      'c-very-dark-blue' : 'hsl(255, 11%, 22%)',
+      'c-dark-violet': 'hsl(257, 27%, 26%)',
+    },
+    // custom fonts
+    fontFamily: {
+      'Poppins': ['Poppins'],
+    },
+    
   },
   plugins: [],
 }
